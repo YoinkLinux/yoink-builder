7aa7478584105d91bf31b729a055a90bacb7d05a
Gavin Pease <gavinpease@gmail.com>
2021-01-09 10:44:15 -0700

    Completely tweaked INSTALL

--- buildpkg.sh
+++ buildpkg.sh
@@ -22,4 +22,29 @@ function log(){
 	echo ${COLOR_INFO} $@;
 }
 
+# If we are downloading a file, the script will download and extract it automatically. 
+# If we are not, we are going to assume we are in the build directory.
+function source_install(){
+	source $PWD/INSTALL
+	version=$name-$major_version.$min_version.$rel_version
+	echo $version
+	if [[ ! -z $download ]]; then
+		get_source
+	fi
+	config
+	build
+	build_install
+	post_build
+}
+
+function cleanup(){
+	unset -f config
+	unset -f build
+	unset -f build_install
+	unset -f post_build
+}
+
+
 [[ ! -f $PWD/INSTALL ]] && error "INSTALL not found!" && exit 1
+source_install
+cleanup
